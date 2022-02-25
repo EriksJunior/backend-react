cca545bf4d7674561afb4d7dd044116064278cbc
EriksJunior <EriksJunior13@gmail.com>
2022-02-24 22:51:06 -0300

    rota para atualizar informações do cliente finalizadas

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -3,13 +3,14 @@ import Cliente from '../../Models/Cliente'
 export default class ClientesController {
   public async index({ params }: HttpContextContract) {
     const page = params.page
-    const data = await Cliente.query().paginate(page, 10)
+    const data = await Cliente.query().paginate(page, 5)
     console.log(data)
     return data
   }
 
   public async store({ request }: HttpContextContract) {
     const data = await request.only([
+      'id',
       'nome',
       'cpf',
       'endereco',
@@ -26,16 +27,29 @@ export default class ClientesController {
   }
 
   public async fetchCustomerToEdit({ params }: HttpContextContract) {
-    const data = await Cliente.findOrFail(params)
+    const data = await Cliente.findOrFail(params.id)
     console.log(data)
     return data
   }
 
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
+  public async update({ request, params }: HttpContextContract) {
+    const data = await Cliente.findOrFail(params.id)
+    const cliente = await request.only([
+      'id',
+      'nome',
+      'cpf',
+      'endereco',
+      'bairro',
+      'numero',
+      'cidade',
+      'uf',
+      'cep',
+      'funcao',
+      'nivel',
+    ])
+    data.merge(cliente)
+    data.save()
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
--- start/routes/cliente.ts
+++ start/routes/cliente.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/cliente/:page', 'ClientesController.index')
 Route.get('/cliente/edit/:id', 'ClientesController.fetchCustomerToEdit')
+Route.put('/cliente/:id', 'ClientesController.update')
 Route.post('/cliente', 'ClientesController.store')
