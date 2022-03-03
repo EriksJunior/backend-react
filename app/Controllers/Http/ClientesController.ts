bbf5c72fc6da9462801f43639e6364336d6f1b90
EriksJunior <EriksJunior13@gmail.com>
2022-03-02 22:26:34 -0300

    criado validator para Cliente e aplicado no ClientesController

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -1,53 +1,31 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Cliente from '../../Models/Cliente'
+import { ClienteValidatorStore, ClienteValidatorUpdate } from 'App/Validators/ClienteValidator'
+
 export default class ClientesController {
   public async index({ params }: HttpContextContract) {
     const page = params.page
     const data = await Cliente.query().paginate(page, 5)
-    console.log(data)
     return data
   }
 
   public async store({ request }: HttpContextContract) {
-    const data = await request.only([
-      'nome',
-      'cpf',
-      'endereco',
-      'bairro',
-      'numero',
-      'cidade',
-      'uf',
-      'cep',
-      'funcao',
-      'nivel',
-    ])
+    const data = await request.validate(ClienteValidatorStore)
     const dataCliente = await Cliente.create(data)
     return dataCliente
   }
 
   public async fetchCustomerToEdit({ params }: HttpContextContract) {
     const data = await Cliente.findOrFail(params.id)
-    console.log(data)
     return data
   }
 
   public async update({ request, params }: HttpContextContract) {
     const data = await Cliente.findOrFail(params.id)
-    const cliente = await request.only([
-      'id',
-      'nome',
-      'cpf',
-      'endereco',
-      'bairro',
-      'numero',
-      'cidade',
-      'uf',
-      'cep',
-      'funcao',
-      'nivel',
-    ])
+    const cliente = await request.validate(ClienteValidatorUpdate)
     data.merge(cliente)
     data.save()
+    return data
   }
 
   public async destroy({ params }: HttpContextContract) {
---
+++ app/Validators/ClienteValidator.ts
@@ -0,0 +1,45 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+class ClienteValidatorStore {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    nome: schema.string({ trim: true }, [rules.maxLength(100)]),
+    cpf: schema.string({ trim: true }, [
+      rules.unique({ table: 'clientes', column: 'cpf' }),
+      rules.maxLength(14),
+    ]),
+    endereco: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
+    bairro: schema.string.optional({ trim: true }, [rules.maxLength(70)]),
+    numero: schema.number.optional(),
+    cidade: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
+    uf: schema.string.optional({ trim: true }, [rules.maxLength(2)]),
+    cep: schema.string.optional({ trim: true }, [rules.maxLength(9)]),
+    funcao: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
+    nivel: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
+  })
+
+  public messages = {}
+}
+
+class ClienteValidatorUpdate {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    nome: schema.string({ trim: true }, [rules.maxLength(100)]),
+    cpf: schema.string({ trim: true }, [rules.maxLength(14)]),
+    endereco: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
+    bairro: schema.string.optional({ trim: true }, [rules.maxLength(70)]),
+    numero: schema.number.optional(),
+    cidade: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
+    uf: schema.string.optional({ trim: true }, [rules.maxLength(2)]),
+    cep: schema.string.optional({ trim: true }, [rules.maxLength(9)]),
+    funcao: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
+    nivel: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
+  })
+
+  public messages = {}
+}
+
+export { ClienteValidatorStore, ClienteValidatorUpdate }
