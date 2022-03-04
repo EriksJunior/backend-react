cfa6ce7a40b6e4d14ac0af25488f57a4828330f8
EriksJunior <EriksJunior13@gmail.com>
2022-03-03 23:13:43 -0300

    implementado messages de errors

--- app/Validators/SuporteValidator.ts
+++ app/Validators/SuporteValidator.ts
@@ -9,18 +9,24 @@ class SuporteValidatorStore {
     cpf: schema.string({ trim: true }, [
       rules.unique({ table: 'suportes', column: 'cpf' }),
       rules.maxLength(14),
+      rules.minLength(14),
     ]),
     endereco: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
     bairro: schema.string.optional({ trim: true }, [rules.maxLength(70)]),
     numero: schema.number.optional(),
     cidade: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
-    uf: schema.string.optional({ trim: true }, [rules.maxLength(2)]),
-    cep: schema.string.optional({ trim: true }, [rules.maxLength(9)]),
+    uf: schema.string.optional({ trim: true }, [rules.maxLength(2), rules.minLength(2)]),
+    cep: schema.string.optional({ trim: true }, [rules.maxLength(9), rules.minLength(9)]),
     funcao: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
     nivel: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
   })
 
-  public messages = {}
+  public messages = {
+    'required': 'O Campo {{field}} deve ser preenchido',
+    'cpf.unique': 'CPF já existente na base de dados',
+    'maxLength': 'O Campo {{field}} pode ter no maximo {{options.maxLength }} caracteres',
+    'minLength': 'O campo {{field}} deve ter no minimo {{options.minLength }} caracteres',
+  }
 }
 
 class SuporteValidatorUpdate {
@@ -28,18 +34,22 @@ class SuporteValidatorUpdate {
 
   public schema = schema.create({
     nome: schema.string({ trim: true }, [rules.maxLength(100)]),
-    cpf: schema.string({ trim: true }, [rules.maxLength(14)]),
+    cpf: schema.string({ trim: true }, [rules.maxLength(14), rules.minLength(14)]),
     endereco: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
     bairro: schema.string.optional({ trim: true }, [rules.maxLength(70)]),
     numero: schema.number.optional(),
     cidade: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
-    uf: schema.string.optional({ trim: true }, [rules.maxLength(2)]),
-    cep: schema.string.optional({ trim: true }, [rules.maxLength(9)]),
+    uf: schema.string.optional({ trim: true }, [rules.maxLength(2), rules.minLength(2)]),
+    cep: schema.string.optional({ trim: true }, [rules.maxLength(9), rules.minLength(9)]),
     funcao: schema.string.optional({ trim: true }, [rules.maxLength(50)]),
     nivel: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
   })
 
-  public messages = {}
+  public messages = {
+    required: 'O Campo {{field}} deve ser preenchido',
+    maxLength: 'O Campo {{field}} pode ter no maximo {{options.maxLength }} caracteres',
+    minLength: 'O campo {{field}} deve ter no minimo {{options.minLength }} caracteres',
+  }
 }
 
 export { SuporteValidatorStore, SuporteValidatorUpdate }
