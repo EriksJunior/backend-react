c9e2378b9b5dae4ed5c8564a85f789f4a6d15328
EriksJunior <EriksJunior13@gmail.com>
2022-03-15 02:29:56 -0300

    commit

--- database/migrations/1647320327125_agenda.ts
+++ database/migrations/1647320327125_agenda.ts
@@ -5,16 +5,26 @@ export default class Agenda extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments('id').primary().unique()
       table
-        .increments('id_cliente')
+        .integer('id_suporte')
         .unsigned()
         .references('id')
         .inTable('suportes')
         .onUpdate('CASCADE')
         .notNullable()
+      table
+        .integer('id_cliente')
+        .unsigned()
+        .references('id')
+        .inTable('clientes')
+        .onUpdate('CASCADE')
+        .notNullable()
       table.dateTime('data_inicio').notNullable()
       table.dateTime('data_fim')
+      table.text('descricao', 'longtext')
+      table.string('situacao_atendimento', 200).notNullable().defaultTo('andamento')
+      table.string('tipo_atendimento', 200).notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
