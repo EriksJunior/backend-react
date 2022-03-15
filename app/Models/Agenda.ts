62541d058ac0e7da3d829cf26554e5b273b871d1
EriksJunior <EriksJunior13@gmail.com>
2022-03-15 02:40:09 -0300

    commit

--- app/Models/Agenda.ts
+++ app/Models/Agenda.ts
@@ -1,9 +1,34 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Suporte from './Suporte'
 
 export default class Agenda extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column({ isPrimary: true })
+  public idSuporte: number
+
+  @belongsTo(() => Suporte, { foreignKey: 'id_suporte' })
+  public suporte: BelongsTo<typeof Suporte>
+
+  @column({ isPrimary: true })
+  public idCliente: number
+
+  @column()
+  public dataInicio: DateTime
+
+  @column()
+  public dataFim: DateTime
+
+  @column()
+  public descricao: string
+
+  @column()
+  public situacaoAtendimento: string
+
+  @column()
+  public tipoAtendimento: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
