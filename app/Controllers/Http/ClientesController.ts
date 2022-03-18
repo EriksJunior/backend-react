18da79e2510fa0a7c7973c5f83c3ea43254dffdc
EriksJunior <EriksJunior13@gmail.com>
2022-03-17 21:18:13 -0300

    commit

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -1,11 +1,24 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Cliente from '../../Models/Cliente'
 
 export default class ClientesController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = await request.only([
+      'nome',
+      'razaoSocial',
+      'endereco',
+      'bairro',
+      'cidade',
+      'numero',
+      'uf',
+      'cep',
+      'cpf_cnpj',
+    ])
 
-  public async store({}: HttpContextContract) {}
+    return await Cliente.create(data)
+  }
 
   public async show({}: HttpContextContract) {}
 
