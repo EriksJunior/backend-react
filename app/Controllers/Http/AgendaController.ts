6f74c9b9a00999b52be591dbe1f79031f57d4d72
EriksJunior <EriksJunior13@gmail.com>
2022-04-03 17:35:44 -0300

    commit

--- app/Controllers/Http/AgendaController.ts
+++ app/Controllers/Http/AgendaController.ts
@@ -1,9 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import Agenda from '../../Models/Agenda'
 export default class AgendaController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async create({request}: HttpContextContract) {
+    const entityAgenda = await request.only(['id_suporte', 'id_cliente', 'data_agendamento','data_inicio', 'data_fim', 'descricao', 'situacao_atendimento', 'tipo_atendimento'])
+    const data = await Agenda.create(entityAgenda)
+    return data
+  }
 
   public async store({}: HttpContextContract) {}
 
